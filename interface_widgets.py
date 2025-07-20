d4eb1e5ad61e64808ceab116605a986ce092a83e
jeanheberth <jeanheberth19@gmail.com>
2025-07-20 13:57:11 -0300

    Realizado a criacao do botao checkout branch

--- interface_widgets.py
+++ interface_widgets.py
@@ -1,5 +1,6 @@
 def construir_interface(janela, repo_var, selecionar_repositorio, acao_atualizar_branch_principal,
-                        acao_criar_branch, acao_commit, acao_commit_push, acao_resolver_conflitos, log_output):
+                        acao_criar_branch, acao_commit, acao_commit_push, acao_resolver_conflitos,
+                        acao_checkout_branch, log_output):
     import tkinter as tk
     tk.Label(janela, text="Repositório Git:").pack(pady=5)
     tk.Entry(janela, textvariable=repo_var, width=60, state="readonly").pack(padx=10)
@@ -10,6 +11,7 @@ def construir_interface(janela, repo_var, selecionar_repositorio, acao_atualizar
     tk.Button(janela, text="Fazer Commit", command=acao_commit, width=40).pack(pady=5)
     tk.Button(janela, text="Commit + Push", command=acao_commit_push, width=40).pack(pady=5)
     tk.Button(janela, text="Resolver Conflitos", command=acao_resolver_conflitos, width=40).pack(pady=5)
+    tk.Button(janela, text="Checkout de Branch", command=acao_checkout_branch, width=40).pack(pady=5)
     tk.Button(janela, text="Sair", command=janela.quit, width=40).pack(pady=20)
 
     log_output.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
