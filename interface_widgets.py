a232de5fb9f87b8e2f346f3feba0743a08888bf3
jeanheberth <jeanheberth19@gmail.com>
2025-08-01 14:46:51 -0300

    Comentado alguns botões que estão dando erro.

--- interface_widgets.py
+++ interface_widgets.py
@@ -1,7 +1,7 @@
 def construir_interface(janela, repo_var, selecionar_repositorio, acao_atualizar_branch_principal,
                         acao_criar_branch, acao_commit, acao_commit_push, acao_resolver_conflitos,
-                        acao_checkout_branch, acao_deletar_branch, acao_criar_pr,
-                        acao_deletar_branch_local, acao_deletar_branch_remota, acao_merge_pull_request, log_output):
+                        acao_checkout_branch,  acao_criar_pr,
+                        acao_deletar_branch_local, acao_deletar_branch_remota,  log_output):
     import tkinter as tk
     tk.Label(janela, text="Repositório Git:").pack(pady=5)
     tk.Entry(janela, textvariable=repo_var, width=60, state="readonly").pack(padx=10)
@@ -13,9 +13,9 @@ def construir_interface(janela, repo_var, selecionar_repositorio, acao_atualizar
     tk.Button(janela, text="Fazer Commit", command=acao_commit, width=40).pack(pady=5)
     tk.Button(janela, text="Commit + Push", command=acao_commit_push, width=40).pack(pady=5)
     tk.Button(janela, text="Criar Pull Request", command=acao_criar_pr, width=40).pack(pady=5)
-    tk.Button(janela, text="Merge Pull Request", command=acao_merge_pull_request, width=40).pack(pady=5)
+    # tk.Button(janela, text="Merge Pull Request", command=acao_merge_pull_request, width=40).pack(pady=5)
     tk.Button(janela, text="Resolver Conflitos", command=acao_resolver_conflitos, width=40).pack(pady=5)
-    tk.Button(janela, text="Deletar Branch", command=acao_deletar_branch, width=40).pack(pady=5)
+    # tk.Button(janela, text="Deletar Branch", command=acao_deletar_branch, width=40).pack(pady=5)
     tk.Button(janela, text="Deletar Branch Local", command=acao_deletar_branch_local, width=40).pack(pady=5)
     tk.Button(janela, text="Deletar Branch Remota", command=lambda: criar_toplevel_deletar_branch_remota(acao_deletar_branch_remota), width=40).pack(pady=5)
 
